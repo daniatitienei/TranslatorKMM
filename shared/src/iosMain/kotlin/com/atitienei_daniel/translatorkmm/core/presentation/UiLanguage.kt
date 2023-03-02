d5443311699d462efb40bbe04c53779d6c13c387
daniatitienei <daniatitienei@gmail.com>
2023-03-02 22:23:31 +0200

    TranslateViewModel

---
+++ shared/src/iosMain/kotlin/com/atitienei_daniel/translatorkmm/core/presentation/UiLanguage.kt
@@ -0,0 +1,24 @@
+package com.atitienei_daniel.translatorkmm.core.presentation
+
+import com.atitienei_daniel.translatorkmm.core.domain.language.Language
+
+actual class UiLanguage(
+    actual val language: Language,
+    val imageName: String
+) {
+
+    actual companion object {
+        actual fun byCode(langCode: String): UiLanguage {
+            return allLanguages.find { it.language.langCode == langCode }
+                ?: throw IllegalArgumentException("Invalid or unsupported language code")
+        }
+
+        actual val allLanguages: List<UiLanguage>
+            get() = Language.values().map { language ->
+                UiLanguage(
+                    language = language,
+                    imageName = language.langName.lowercase()
+                )
+            }
+    }
+}
