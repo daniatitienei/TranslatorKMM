c929e0f694756196a94fab510e05046a8e30dd92
daniatitienei <daniatitienei@gmail.com>
2023-03-04 00:22:21 +0200

    translation text field

---
+++ androidApp/src/main/java/com/atitienei_daniel/translatorkmm/android/translate/presentation/components/LanguageDisplay.kt
@@ -0,0 +1,30 @@
+package com.atitienei_daniel.translatorkmm.android.translate.presentation.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.atitienei_daniel.translatorkmm.android.core.theme.LightBlue
+import com.atitienei_daniel.translatorkmm.core.presentation.UiLanguage
+
+@Composable
+fun LanguageDisplay(
+    language: UiLanguage,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        SmallLanguageIcon(language = language)
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = language.language.langName,
+            color = LightBlue
+        )
+    }
+}
