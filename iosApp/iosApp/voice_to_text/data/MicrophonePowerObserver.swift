099cf84eef56ffc59682d2bd1abddfd0d8371232
daniatitienei <daniatitienei@gmail.com>
2023-03-08 11:02:05 +0200

    voice to text parser

---
+++ iosApp/iosApp/voice_to_text/data/MicrophonePowerObserver.swift
@@ -0,0 +1,71 @@
+//
+//  MicrophonePowerObserver.swift
+//  iosApp
+//
+//  Created by Atitienei Daniel on 06.03.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import shared
+import Speech
+import Combine
+
+class MicrophonePowerObserver: ObservableObject {
+    private var cancellable: AnyCancellable? = nil
+    private var audioRecorder: AVAudioRecorder? = nil
+    
+    @Published private(set) var micPowerRatio = 0.0
+    
+    private let powerEmissionsPerSecond = 20.0
+    
+    func startObserving() {
+        do {
+            let recorderSettings: [String: Any] = [
+                AVFormatIDKey: NSNumber(value: kAudioFormatAppleLossless),
+                AVNumberOfChannelsKey: 1
+            ]
+            
+            let recorder = try AVAudioRecorder(
+                url: URL(
+                    fileURLWithPath: "/dev/null",
+                    isDirectory: true
+                ),
+                settings: recorderSettings
+            )
+            recorder.isMeteringEnabled = true
+            recorder.record()
+            self.audioRecorder = recorder
+            
+            self.cancellable = Timer.publish(
+                every: 1.0 / powerEmissionsPerSecond,
+                tolerance: 1.0 / powerEmissionsPerSecond,
+                on: .main,
+                in: .common
+            )
+            .autoconnect()
+            .sink { [weak self] _ in
+                recorder.updateMeters()
+                
+                let powerOffset = recorder.averagePower(forChannel: 0)
+                if powerOffset < -50 {
+                    self?.micPowerRatio = 0.0
+                } else {
+                    let normalizedOffset = CGFloat(50 + powerOffset) / 50
+                    self?.micPowerRatio = normalizedOffset
+                }
+            }
+        } catch {
+            print("An error occured when observing microphone power: \(error.localizedDescription)")
+        }
+    }
+    
+    func release() {
+        cancellable = nil
+        
+        audioRecorder?.stop()
+        audioRecorder = nil
+        
+        micPowerRatio = 0.0
+    }
+}
